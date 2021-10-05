38a40e985c17a9680437a77a80873f2a3b6dc3e4
Parris Greaves <parris.greaves@gmail.com>
2021-10-05 16:11:03 -0400

    worked out the logic for the composer-char-counter, including turning the text red if over 140 char

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,25 @@
-console.log("Yes, composer-char is linked correctly");
+//Ensure the DOM has loaded
+$(document).ready(function() {
+  
+  $("#tweet-text").on('input', function() {
+    let tweetLength = $(this).val().length;
+    let charRemaining = 140 - tweetLength;
+    console.log(charRemaining);
+    $(this).parent().find(".counter").css("color", "black");
+    if (charRemaining < 0) {
+      $(this).parent().find(".counter").css("color", "red");
+    }
+    console.log($(this).parent().find(".counter"));
+    $(this).parent().find(".counter").text(charRemaining);
+
+
+  })
+
+
+
+
+
+
+
+
+});
