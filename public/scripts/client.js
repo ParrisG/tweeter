add5fde73889a40380cb432d3bd3c64dc8a7ba80
Parris Greaves <parris.greaves@gmail.com>
2021-10-06 14:47:48 -0400

    cleaned up some of the test and driver code from client.jss

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,18 +5,6 @@
  */
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [{
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }]
 
   const getTweets = function() {
     $.get("/tweets/", function(data) {
@@ -65,7 +53,6 @@ $(document).ready(function() {
 
 
 
-
   // Handling the actual submission of the new tweet to the server
   const $newTweetForm = $("#new-tweet");
   $newTweetForm.on("submit", function(event) {
@@ -77,20 +64,6 @@ $(document).ready(function() {
     });
 
   })
-
-
-
-
-
-  // Test / driver code (temporary)
-  renderTweets(tweetData);
-
-  // // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
 
 });
 
